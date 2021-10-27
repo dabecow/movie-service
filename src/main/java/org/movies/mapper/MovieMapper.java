c3ee5f4a6e10dc0e10a61809d3dff1f5e04e8cb5
Andrei Bikov <andrei_bikov@senla.eu>
2021-10-27 18:00:57 +0300

    MVP fix

--- src/main/java/org/movies/mapper/MovieMapper.java
+++ src/main/java/org/movies/mapper/MovieMapper.java
@@ -11,15 +11,9 @@ import java.util.List;
 @Mapper(componentModel = "Spring")
 public interface MovieMapper {
 
-    @Mappings({
-            @Mapping(target = "releaseDate", source = "movie.releaseDate", dateFormat = "dd-MM-yyyy")
-    })
     MovieDto toDto(Movie movie);
 
     List<MovieDto> toDtoList(List<Movie> movies);
 
-    @Mappings({
-            @Mapping(target = "releaseDate", source = "dto.releaseDate", dateFormat = "dd-MM-yyyy")
-    })
     Movie fromDto(MovieDto dto);
 }
